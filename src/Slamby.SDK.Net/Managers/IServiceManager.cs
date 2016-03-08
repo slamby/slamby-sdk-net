bab82a25e0f4dfa6b3ba9100d40fc41bc52799fc
zsolt.illes <zsolt.illes@slamby.com>
2016-03-08 10:25:55 +0100

    Added ServiceManager

---
+++ src/Slamby.SDK.Net/Managers/IServiceManager.cs
@@ -0,0 +1,15 @@
+﻿using Slamby.SDK.Net.Models;
+using Slamby.SDK.Net.Models.Services;
+using System.Threading.Tasks;
+
+namespace Slamby.SDK.Net.Managers
+{
+    public interface IServiceManager
+    {
+        Task<ClientResponse> CreateServiceAsync(Service service);
+        Task<ClientResponse> DeleteServiceAsync(string serviceId);
+        Task<ClientResponseWithObject<Service>> GetServiceAsync(string serviceId);
+        Task<ClientResponseWithObject<Service>> GetServicesAsync();
+        Task<ClientResponse> UpdateServiceAsync(string serviceId, Service service);
+    }
+}
