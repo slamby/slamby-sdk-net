47ed49789abe261122f6702da817bbca69885439
Attila Laszlo <attila.laszlo@slamby.com>
2016-03-11 11:20:47 +0100

    Classifier Service related models + Process model

---
+++ src/Slamby.SDK.Net/Models/Services/ClassifierService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slamby.SDK.Net.Models.Services
+{
+    public class ClassifierService : Service
+    {
+        public ClassifierPrepareSettings PrepareSettings { get; set; }
+
+        public ClassifierActivateSettings ActivateSettings { get; set; }
+    }
+}
