033e9601e4c47d62f70ec2ff924b0439f35ec5f1
Attila Laszlo <attila.laszlo@slamby.com>
2016-03-18 08:53:22 +0100

    prc first steps

--- src/Slamby.SDK.Net/Models/Services/Prc/PrcService.cs
+++ src/Slamby.SDK.Net/Models/Services/Prc/PrcService.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Slamby.SDK.Net.Models.Enums
+namespace Slamby.SDK.Net.Models.Services
 {
-    public enum ServiceTypeEnum
+    public class PrcService : Service
     {
-        Classifier,
-        Prc
+
     }
 }
