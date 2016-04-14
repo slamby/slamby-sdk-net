405c3eccf6dd757cb75875ab919e4e9a1e7e7d55
Attila Laszlo <attila.laszlo@slamby.com>
2016-04-14 12:28:23 +0200

    namespace fix

--- src/Slamby.SDK.Net/Models/Services/Classifier/ClassifierExportDictionariesSettings.cs
+++ src/Slamby.SDK.Net/Models/Services/Classifier/ClassifierExportDictionariesSettings.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Slamby.SDK.Net.Models.Services.Classifier
+namespace Slamby.SDK.Net.Models.Services
 {
     public class ClassifierExportDictionariesSettings
     {
