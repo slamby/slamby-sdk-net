5146a60f4ecf720b6a57004c21241b9737e938fd
zsolt.illes <zsolt.illes@slamby.com>
2016-04-19 13:12:14 +0200

    Added status endpoint

---
+++ src/Slamby.SDK.Net/Managers/StatusManager.cs
@@ -0,0 +1,21 @@
+﻿using Slamby.SDK.Net.Models;
+using System.Threading.Tasks;
+
+namespace Slamby.SDK.Net.Managers
+{
+    public class StatusManager : BaseManager
+    {
+        private static readonly string Endpoint = "api/status";
+
+        public StatusManager(Configuration config) 
+            : base(config, Endpoint)
+        {
+            _configuration = config;
+        }
+
+        public async Task<ClientResponseWithObject<Status>> GetStatusAsync()
+        {
+            return await _client.SendAsync<Status>(System.Net.Http.HttpMethod.Get, null, null, null, null);
+        }
+    }
+}
---
+++ src/Slamby.SDK.Net/Models/Status.cs
@@ -0,0 +1,17 @@
+﻿namespace Slamby.SDK.Net.Models
+{
+    public class Status
+    {
+        public int ProcessorCount { get; set; }
+
+        public decimal AvailableFreeSpace { get; set; }
+
+        public string ApiVersion { get; set; }
+
+        public decimal CpuUsage { get; internal set; }
+
+        public decimal TotalMemory { get; internal set; }
+
+        public decimal FreeMemory { get; internal set; }
+    }
+}
