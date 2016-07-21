3db38f234eddb018fa02c33ddcc76b49d1caa4c6
zsolt.illes <zsolt.illes@slamby.com>
2016-07-21 13:17:23 +0200

    added HelperManager

---
+++ src/Slamby.SDK.Net/Managers/HelperManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Slamby.SDK.Net.Managers.Interfaces;
+using Slamby.SDK.Net.Models;
+
+namespace Slamby.SDK.Net.Managers
+{
+    public class HelperManager : BaseManager, IHelperManager
+    {
+        private static readonly string Endpoint = "api/helper";
+        private static readonly string FileParserEndpoint = "api/helper/fileparser";
+
+        public HelperManager(Configuration config) 
+            : base(config, Endpoint)
+        {
+            _configuration = config;
+        }
+
+        public async Task<ClientResponseWithObject<Status>> FileParserAsync(FileParser fileParser)
+        {
+            var client = new ApiClient(_configuration, FileParserEndpoint);
+            return await client.SendAsync<Status>(System.Net.Http.HttpMethod.Post, fileParser, null, null, null, true);
+        }
+    }
+}
