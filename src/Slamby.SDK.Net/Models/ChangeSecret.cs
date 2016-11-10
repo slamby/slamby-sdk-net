3544473c65eb8b1412e84a1dc23f6ac7c520393a
zsolt.illes <zsolt.illes@slamby.com>
2016-11-10 10:41:45 +0100

    added ChangeSecret model

---
+++ src/Slamby.SDK.Net/Models/ChangeSecret.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Slamby.SDK.Net.Models
+{
+    /// <summary>
+    /// Change Secret model
+    /// </summary>
+    public class ChangeSecret
+    {
+        /// <summary>
+        /// New secret to be set
+        /// </summary>
+        [Required]
+        public string NewSecret { get; set; }
+    }
+}
