4ed39a5bf7b4f2a272e57e9477d4637ac8bbd76a
Attila Laszlo <attila.laszlo@slamby.com>
2017-01-06 10:45:25 +0100

    statistics manager

---
+++ src/Slamby.SDK.Net/Managers/Interfaces/IStatisticsManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Slamby.SDK.Net.Models;
+
+namespace Slamby.SDK.Net.Managers.Interfaces
+{
+    public interface IStatisticsManager
+    {
+        Task<ClientResponseWithObject<StatisticsWrapper>> GetStatisticsAsync(int year = -1, int month = -1);
+    }
+}
---
+++ src/Slamby.SDK.Net/Managers/StatisticsManager.cs
@@ -0,0 +1,24 @@
+﻿using Slamby.SDK.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slamby.SDK.Net.Managers
+{
+    public class StatisticsManager : BaseManager
+    {
+        private static readonly string Endpoint = "api/statistics";
+
+        public StatisticsManager(Configuration config)
+            : base(config, Endpoint)
+        {
+            _configuration = config;
+        }
+
+        public async Task<ClientResponseWithObject<StatisticsWrapper>> GetStatisticsAsync(int year = -1, int month = -1)
+        {
+            return await _client.SendAsync<StatisticsWrapper>(System.Net.Http.HttpMethod.Get, null, $"{year}/{month}", null, null);
+        }
+    }
+}
